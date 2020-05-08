807fbef7b7bf3f853f65d416aa6f9bca1ef498e6
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-05-08 21:48:35 +0700

    python

---
+++ Python3_Basic/8. Tìm giá trị nhỏ nhất trong List/C1 - Dùng Loop.py
@@ -0,0 +1,17 @@
+n = int( input() )
+lst = []
+for i in range( n ):
+    lst.append( int( input() ) )
+min_value = lst[0]
+for i in lst:
+    if i < min_value:
+        min_value = i
+print( min_value )
+
+'''
+3
+1
+2
+3
+1
+'''
---
+++ Python3_Basic/8. Tìm giá trị nhỏ nhất trong List/C2 - Hàm Min().py
@@ -0,0 +1,13 @@
+n = int( input() )
+lst = []
+for i in range( n ):
+    lst.append( int( input() ) )
+print( min( lst ) )
+
+'''
+3
+1
+2
+3
+1
+'''
