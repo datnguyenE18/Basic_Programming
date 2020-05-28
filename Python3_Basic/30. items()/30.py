ca8ecce81f8723900c332429555450756aafccd0
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-05-28 15:50:45 +0700

    python

--- Python3_Basic/30. items()/30.py
+++ Python3_Basic/30. items()/30.py
@@ -2,19 +2,21 @@ friend = {"Ten": "Nguyen Van A", "Tuoi": 22}
 
 print( friend )
 x = friend.items()
-print( x )
+print( x, '\n' )
 
+# When an item in the dictionary changes value, the view object also gets updated:
 friend["Ten"] = "Tran Van B" # Đổi giá trị của Ten
 friend["Truong"] = "THPT ABC" # Thêm đối tượng và giá trị Truong
-x = friend.items()
-print( friend )
-print( x )
+print( "When an item in the dictionary changes value, the view object also gets updated:" ) 
+print( friend,"\n", x )
 
 '''
 {'Ten': 'Nguyen Van A', 'Tuoi': 22}
 dict_items([('Ten', 'Nguyen Van A'), ('Tuoi', 22)])
+
+When an item in the dictionary changes value, the view object also gets updated:
 {'Ten': 'Tran Van B', 'Tuoi': 22, 'Truong': 'THPT ABC'}
-dict_items([('Ten', 'Tran Van B'), ('Tuoi', 22), ('Truong', 'THPT ABC')])
+ dict_items([('Ten', 'Tran Van B'), ('Tuoi', 22), ('Truong', 'THPT ABC')])
 '''
 
 
