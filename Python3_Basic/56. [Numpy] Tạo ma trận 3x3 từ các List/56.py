f818d3ac76569eba2dc7e7c28ca4fb8cd68b8659
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-06-04 17:19:01 +0700

    Python

---
+++ Python3_Basic/56. [Numpy] Tạo ma trận 3x3 từ các List/56.py
@@ -0,0 +1,17 @@
+﻿# List of lists:
+my_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print( my_list )
+
+# Import Numpy:
+import numpy as np
+
+# Creat matrix from above Lists:
+my_arr = np.array( my_list )
+print( my_arr )
+
+'''
+[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+[[1 2 3]
+ [4 5 6]
+ [7 8 9]]
+'''
---
+++ Python3_Basic/58. [Numpy][zeros and ones] 8. Sinh mảng 0 và mảng 1/58.py
@@ -0,0 +1,38 @@
+﻿# Import Numpy:
+import numpy as np
+
+# Mảng 0 một chiều:
+arr01 = np.zeros( 3 )
+print( arr01 ) #[0.  0.  0.]
+
+# Mảng 0 hai chiều:
+arr02 = np.zeros( ( 5,5 ) )
+arr02 = np.zeros( ( 5,5 ) )
+print( arr02 )
+
+# Mảng 1 một chiều:
+arr11 = np.ones( 3 )
+print( arr11 ) # [1.  1.  1.]
+
+# Mảng 1 hai chiều:
+arr12 = np.ones( ( 5,5 ) )
+print( arr12 )
+
+# Truy cập các mảng trên:
+print( arr01[1] + arr11[2] ) # 1.0
+
+'''
+[0. 0. 0.]
+[[0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0.]]
+[1. 1. 1.]
+[[1. 1. 1. 1. 1.]
+ [1. 1. 1. 1. 1.]
+ [1. 1. 1. 1. 1.]
+ [1. 1. 1. 1. 1.]
+ [1. 1. 1. 1. 1.]]
+1.0
+'''
