593bb9dca088da7f2d42e24e3f3afa0e28c04286
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-08-14 21:44:11 +0700

    14/08/2020

---
+++ Python/3. [Py] Code/47. OOP/9. __doc__/9.py
@@ -0,0 +1,11 @@
+﻿# __doc__ sẽ trả về chuỗi docstring mô tả của lớp đó
+
+class MyClass:
+     "Đây là class thứ 2 được khởi tạo"
+     a = 10
+
+# Output: 10
+print(MyClass.a)
+
+# Output: 'Đây là class thứ 2 được khởi tạo'
+print(MyClass.__doc__)
