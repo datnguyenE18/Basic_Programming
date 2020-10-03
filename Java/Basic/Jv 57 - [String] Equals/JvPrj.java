c61e00db6f815d7f8968844a03256ff895f1fae2
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-10-03 10:01:06 +0700

    03/10/2020

---
+++ Java/Basic/Jv 57 - [String] Equals/JvPrj.java
@@ -0,0 +1,16 @@
+package jvprj;
+
+import java.util.*;
+
+public class JvPrj {
+
+    public static void main( String[] args ) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        if ( a.equals(b) )
+            System.out.println("two people have the same name");
+        else
+            System.out.println("two people don't have the same name");
+    }
+}
---
+++ Java/Basic/Jv58 - [String] toCharArray()/JvPrj.java
@@ -0,0 +1,10 @@
+package jvprj;
+
+public class JvPrj {
+
+    public static void main( String[] args ) {
+        String s = "Hello World";
+        for ( char c : s.toCharArray() )
+            System.out.println(c);
+    }
+}
