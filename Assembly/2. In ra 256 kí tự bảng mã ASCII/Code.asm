f36cfa03fba321d67e0600a7010f221ed12caaae
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-10-05 12:59:23 +0700

    05/10/2020

---
+++ Assembly/2. In ra 256 kí tự bảng mã ASCII/Code.asm
@@ -0,0 +1,19 @@
+.Model Small
+.Stack 100h
+.Data
+.Code
+Start:
+    mov cx, 256
+    mov al, 0
+    
+    lp:
+        mov ah, 2
+        mov dl, al
+        int 21h
+        inc al
+        
+    loop lp
+    mov ah, 4Ch
+    int 21h
+
+End
