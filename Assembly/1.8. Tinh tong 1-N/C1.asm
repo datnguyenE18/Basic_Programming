853cd7fb19290b2f1e31d3135ff16e5dd1c3b991
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-10-13 00:16:25 +0700

    13/10/2020

---
+++ Assembly/1.8. Tinh tong 1-N/C1.asm
@@ -0,0 +1,36 @@
+.Model Small
+
+.Stack 100h 
+
+.Data
+ 
+.Code
+ nhap:
+    MOV Ah,01H       ;nhap 
+    INT 21H          
+    sub al,30h       ;doi ki tu sang so
+
+    mov cl,al         ;cl=al
+    mov ch,0         ;ch=0
+    mov ax,0         ;ax=0
+
+ lap:
+    add ax,cx        ;tinh tong ax += cx       
+ loop lap    
+    mov bx,10        ;bx=10
+
+ chia:
+    mov dx,0
+    div bx           ;ax /= 10 (phan du duoc luu trong dx)
+    inc cx           ;cx+1
+    push dx          ;cat dx vao ngan xep
+    test ax,ax       ;kiem tra ax==0 ?
+    jz ht
+    jmp chia
+
+ ht:
+    pop dx           ;lay ket qua 
+    add dx,30h       ;doi sang ascii
+    mov ah,2         ;in ra kq
+    int 21h
+ loop ht
